4e5f123fa45fb1ca9d3b5ef02424af49201eb784
EXTENSION <brunopaullom@hotmail.com>
2019-05-05 13:59:26 -0500

    05052019 EntityFramework

---
+++ Cap04/slnApp/App.Data.DataAccessTest/ArtistTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using App.Data.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Data.DataAccessTest
+{
+    [TestClass]
+    public class ArtistTest
+    {
+        private readonly ArtistDA da = new ArtistDA();
+
+        [TestMethod]
+        public void GetAll()
+        {
+            var artists = da.GetAll();
+            Assert.IsTrue(artists.Count > 0);
+        }
+    }
+}
